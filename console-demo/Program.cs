0491772f6eeb84767830ba8b87e0af3b8aee08c4
Fredrik Klingenberg <fredrkl@gmail.com>
2025-11-30 21:23:41 +0100

    ✨ Observable demo

--- console-demo/Program.cs
+++ console-demo/Program.cs
@@ -1,5 +1,13 @@
 ﻿using System.Reactive.Linq;
 
+IObservable<int> observable = Observable.Range(1, 5);
+
+observable.Subscribe(
+    onNext: value => Console.WriteLine($"Received value: {value}"),
+    onError: error => Console.WriteLine($"Error occurred: {error.Message}"),
+    onCompleted: () => Console.WriteLine("Sequence completed.")
+);
+
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.ReadLine();

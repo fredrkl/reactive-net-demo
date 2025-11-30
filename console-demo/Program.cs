027aba113ed33c322156869055001ab8b1c7f333
Fredrik Klingenberg <fredrkl@gmail.com>
2025-11-30 09:57:10 +0100

    ✨ Adding reactive extension

--- console-demo/Program.cs
+++ console-demo/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Reactive.Linq;
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.ReadLine();

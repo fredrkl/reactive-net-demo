773505c3edb1b61c51ade9d432085a47d97fee92
Fredrik Klingenberg <fredrkl@gmail.com>
2025-12-03 12:22:02 +0100

    ⚗️ Showcasing using observable list of custom types

--- console-demo/Program.cs
+++ console-demo/Program.cs
@@ -1,9 +1,19 @@
 ﻿using System.Reactive.Linq;
 
-IObservable<int> observable = Observable.Range(1, 5);
+IEnumerable<int> numbers = [1, 2, 3, 4, 5];
+IObservable<int> numbersObservable = numbers.ToObservable();
 
-observable.Subscribe(
-    onNext: value => Console.WriteLine($"Received value: {value}"),
+IObservable<int> observable = Observable.Range(1, 5);
+IObservable<Customer> observableagain = Observable.Create<Customer>(observer =>
+{
+    observer.OnNext(new Customer { Id = 1, Name = "John Doe" });
+    observer.OnNext(new Customer { Id = 2, Name = "Jane Smith" });
+    observer.OnCompleted();
+    return System.Reactive.Disposables.Disposable.Empty;
+});
+
+observableagain.Subscribe(
+    onNext: value => Console.WriteLine($"Received value: {value.Name}"),
     onError: error => Console.WriteLine($"Error occurred: {error.Message}"),
     onCompleted: () => Console.WriteLine("Sequence completed.")
 );
@@ -11,3 +21,9 @@ observable.Subscribe(
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.ReadLine();
+
+internal class Customer
+{
+    public int Id { get; internal set; }
+    public required string Name { get; internal set; }
+}
